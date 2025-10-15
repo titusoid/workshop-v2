1847059c30e9525a7b5adf246ea43ad2e131b6a9
Stefan Vucak <stefan.vucak@partner.bmwgroup.com>
2025-10-15 13:37:29 +0200

    Add actions

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  env: { browser: true, es2023: true },
+  extends: ["eslint:recommended"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  rules: { "no-unused-vars": "error", "no-undef": "error" }
+};
